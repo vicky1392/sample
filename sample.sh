cc7856fcd8f11f3da37af2a38f103a444fe13668
vicky1392 <103995877+vicky1392@users.noreply.github.com>
2022-05-14 18:41:44 +0530

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -6,5 +6,9 @@ echo "todays date"
 echo date --date="tomorrow"
 echo date --date="next monday"
 echo date +"Year: %Y, Month: %m, Day: %d"
+def today = new Date()
+def yesterday = today - 1
+println today.format("MM/dd/yyyy")
+println yesterday.format("MM/dd/yyyy")
 
 
